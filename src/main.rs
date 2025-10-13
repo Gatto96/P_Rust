4bfd87c2f6ab5a486c037a791d27e6948ee8b062
Fernando Salas <fer.ivan.gatto96@gmail.com>
2025-10-12 19:00:27 -0600

    loops

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ fn main() {
 
 
 
-    let mut i=0;
-    while i<5{
+    // let mut i=0;
+    // while i<5{
+    //     println!("i={}",i);
+    //     i+=1;
+    // }
+
+    for i in 1..=10{
         println!("i={}",i);
-        i+=1;
+
     }
+
 
 
 
