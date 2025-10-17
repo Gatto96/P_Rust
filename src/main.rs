63e1ad8d53a9f3ed6095c8e9e4cbec0cf590a76d
Fernando Salas <fer.ivan.gatto96@gmail.com>
2025-10-17 15:59:16 -0600

    lesson 10

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+
+
 fn main() {
     println!("Hello, rusters!");
 
@@ -18,12 +21,47 @@ fn main() {
     //     println!("{}",i);
     // }
 
-    let number =vec![1,2,3,4,5];
-    for n in number{
-        println!("{}",n);
+    // let number =vec![1,2,3,4,5];
+    // for n in number{
+    //     println!("{}",n);
+    // }
+
+    // for i in 1..=10{
+    //     if i % 2 ==0 {
+    //         //skip even numbers
+    //         continue; //mean skipping numbers
+    //     }
+    //     println!("i = {}", i);
+    //     if i == 7 {
+    //         //exit loop when is 7
+    //         break; //stop;exit
+    //     }
+    // }
+
+    //Match control flow
+
+    // let name = "Hello";
+
+    // //use of match to pattern match against varible "name"
+    // match name {
+    //     "Good bye" => println! ("Sorry to see you go"),
+    //     "Hello" => println! ("Hi, Ruster, nice to meet you"),
+    //     _ => println! ("I can't find a greeting, good bye"),
+    // }
+
+    println!("Please enter a greeting: ");
+    let mut name= String::new();
+    io::stdin().read_line(&mut name).expect("Failed to read input");
+
+    //use of match expression to pattern against varible "name"
+    match name.trim(){
+        "Good bye" => println! ("Sorry to see you go."),
+        "Hello"=> println! ("Hi, Ruster, glad to meet you"),
+        _ => println! ("I can't find a greeting, good bye"),
     }
 
-
+    
+    
 
 
 }
